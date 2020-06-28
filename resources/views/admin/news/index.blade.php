f042f38bc5be59c80d2a8aea9416e2324bfd225b
Nakamu <yuuki.nakamura.0828@gmail.com>
2020-06-28 21:22:00 +0800

    [管理画面] ニュース管理のベースを作成

---
+++ resources/views/admin/news/index.blade.php
@@ -0,0 +1,15 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <div>
+        <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+
+            <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
+                ニュース一覧
+            </div>
+
+            <div class="w-full p-6">
+            </div>
+        </div>
+    </div>
+@endsection
